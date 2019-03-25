c3c0aeffd8383ac1d022cb8367579ef255532873
Chilllli <justin.tischer@web.de>
2019-03-25 20:03:03 +0100

    troll feature

--- Discord-Chan/commands/AdminCommands.cs
+++ Discord-Chan/commands/AdminCommands.cs
@@ -19,13 +19,27 @@ namespace Discord_Chan.commands
                 //check if the user, which has written the message, has admin rights
                 if((Context.Message.Author as SocketGuildUser).Roles.ToList().Find(r => r.Id == Program.BotConfiguration.AdminRole) != null)
                 {
-                    //user has admin rights
-                    await Context.Message.Channel.SendMessageAsync($"{userId} => {Program.MyGuild.Users.ToList().Find(u => u.Id == userId)}");
-                }
-                else
-                {
                     //user has not admin rights
                     await Context.Message.Channel.SendMessageAsync($"{Context.Message.Author.Username}, you have no rights to do that.");
+                }
+                //user has admin rights
+                await Context.Message.Channel.SendMessageAsync($"{userId} => {Program.MyGuild.Users.ToList().Find(u => u.Id == userId)}");
+            }
+            [Command("reverse")]
+            public async Task ReverseUsernames()
+            {
+                //check if the user, which has written the message, has admin rights
+                if ((Context.Message.Author as SocketGuildUser).Roles.ToList().Find(r => r.Id == Program.BotConfiguration.AdminRole) == null)
+                {
+                   //user has not admin rights
+                    await Context.Message.Channel.SendMessageAsync($"{Context.Message.Author.Username}, you have no rights to do that.");
+                }
+                foreach (SocketGuildUser guildUser in Program.MyGuild.Users)
+                {
+                    //"remove" owner
+                    if (guildUser.Id == Program.MyGuild.OwnerId)
+                        continue;
+                    await guildUser.ModifyAsync(u => u.Nickname = new String((guildUser.Nickname != null ? guildUser.Nickname : guildUser.Username).Reverse().ToArray())); 
                 }
             }
         }
