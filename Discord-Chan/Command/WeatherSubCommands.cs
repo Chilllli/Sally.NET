389437006a71ba974fa5dd5ee6b49473438fb65c
Chilllli <justin.tischer@web.de>
2019-04-08 21:44:53 +0200

    custom weather command

--- Discord-Chan/Command/WeatherSubCommands.cs
+++ Discord-Chan/Command/WeatherSubCommands.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord_Chan.Config;
 using Discord_Chan.Db;
 using Newtonsoft.Json;
@@ -47,6 +48,32 @@ namespace Discord_Chan.Command
             currentUser.WeatherLocation = null;
             currentUser.NotifierTime = null;
             await Context.Message.Channel.SendMessageAsync($"{Context.Message.Author}, you successfully unsubbed to weather notifications.");
+        }
+        [Command("currentWeather")]
+        public async Task CheckCurrentWeather(string location)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.openweathermap.org");
+            HttpResponseMessage response = await client.GetAsync($"/data/2.5/weather?q={HttpUtility.UrlEncode(location, Encoding.UTF8)}&appid={Program.BotConfiguration.WeatherApiKey}&units=metric");
+
+            string stringResult = await response.Content.ReadAsStringAsync();
+
+            dynamic temperature = JsonConvert.DeserializeObject<dynamic>(stringResult);
+            if (temperature.cod != 200)
+            {
+                await Context.Message.Channel.SendMessageAsync((string)temperature.message);
+                return;
+            }
+            EmbedBuilder weatherEmbed = new EmbedBuilder()
+                    .WithTitle("Weather Info")
+                    .WithDescription("Current Weather Informations")
+                    .AddField(location, $"{temperature.main.temp} °C")
+                    .AddField("Current Max. Temp", $"{temperature.main.temp_max} °C")
+                    .AddField("Current Min. Temp", $"{temperature.main.temp_min} °C")
+                    .AddField("Current Weather Condition", (string)temperature.weather[0].main)
+                    .WithColor(Color.Blue)
+                    .WithTimestamp(DateTime.Now);
+            await Context.Message.Channel.SendMessageAsync(embed: weatherEmbed.Build()).ConfigureAwait(false);
         }
     }
 }
