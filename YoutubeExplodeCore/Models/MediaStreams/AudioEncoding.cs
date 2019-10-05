466b8521dec291ea69c57bca44733750c4297364
ItsChillli_ <justin.tischer@web.de>
2019-10-05 14:34:35 +0200

    adding YoutubeExplodeCore

---
+++ YoutubeExplodeCore/Models/MediaStreams/AudioEncoding.cs
@@ -0,0 +1,23 @@
+﻿namespace YoutubeExplode.Models.MediaStreams
+{
+    /// <summary>
+    /// Audio encoding.
+    /// </summary>
+    public enum AudioEncoding
+    {
+        /// <summary>
+        /// MPEG-4 Part 3, Advanced Audio Coding (AAC).
+        /// </summary>
+        Aac,
+
+        /// <summary>
+        /// Vorbis.
+        /// </summary>
+        Vorbis,
+
+        /// <summary>
+        /// Opus.
+        /// </summary>
+        Opus
+    }
+}
---
+++ YoutubeExplodeCore/YoutubeClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using YoutubeExplode.Internal;
+
+namespace YoutubeExplode
+{
+    /// <summary>
+    /// The entry point for <see cref="YoutubeExplode"/>.
+    /// </summary>
+    public partial class YoutubeClient : IYoutubeClient
+    {
+        private readonly HttpClient _httpClient;
+
+        /// <summary>
+        /// Creates an instance of <see cref="YoutubeClient"/>.
+        /// </summary>
+        public YoutubeClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient.GuardNotNull(nameof(httpClient));
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="YoutubeClient"/>.
+        /// </summary>
+        public YoutubeClient()
+            : this(HttpClientEx.GetSingleton())
+        {
+        }
+    }
+}
